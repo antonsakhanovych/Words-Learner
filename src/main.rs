37c28bbf1a28e33b8637aff73ff1a2cf087c9620
Anton Sakhanovych <antonsakhanovych@gmail.com>
2024-03-28 15:10:19 -0400

    parse file path from args

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,25 @@ mod app;
 mod errors;
 mod tui;
 
-use color_eyre::Result;
+use color_eyre::{eyre::OptionExt, Result};
+use std::{env, mem};
 
-const FILE_PATH: &'static str = "words.json";
+fn parse_args() -> Option<String> {
+    let mut args: Vec<String> = env::args().collect();
+    if let Some(arg) = args.get_mut(1) {
+        Some(mem::take(arg))
+    } else {
+        None
+    }
+}
 
 fn main() -> Result<()> {
     errors::install_hooks()?;
+    let file_path = parse_args().ok_or_eyre("ERROR: Please provide file path!")?;
     let mut terminal = tui::enable()?;
     terminal.clear()?;
 
-    let mut app = app::App::new(FILE_PATH)?;
+    let mut app = app::App::new(&file_path)?;
     app.run(&mut terminal)?;
 
     tui::restore()?;
